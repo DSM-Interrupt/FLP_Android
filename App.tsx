4c2b541526c1b0f85812ba077a0171a55a679dc2
SJY <jihoseo2008@gmail.com>
2025-04-22 12:21:31 +0900

    feat :: role select page
    add page ( select admin or member role page )

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
+import Start from "./app/screens/start"
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native"
 
 export default function App() {
+    const Stack = createStackNavigator()
+
     return (
-        <View style={styles.container}>
-            <Text>Hello World!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Start">
+                <Stack.Screen name="Start" component={Start} />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-})
