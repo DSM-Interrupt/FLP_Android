facb4867e00430975bb78552e728021613440be7
SJY <jihoseo2008@gmail.com>
2025-04-30 08:39:41 +0900

    feat :: bottom sheet
    add bottom sheet component
    (ft. gorhom/react-native-bottom-sheet)

--- app/screens/member.tsx
+++ app/screens/member.tsx
@@ -1,10 +1,16 @@
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet"
+import MyBottomSheet from "../components/common/BottomSheet"
 import Map from "../components/Map"
+import { Text } from "react-native"
 
 function Member() {
     return (
-        <>
+        <BottomSheetModalProvider>
             <Map />
-        </>
+            <MyBottomSheet>
+                <Text>Hello World!</Text>
+            </MyBottomSheet>
+        </BottomSheetModalProvider>
     )
 }
 
