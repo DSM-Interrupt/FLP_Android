40d30822ba8a5e12237f33c71836e46e06b5e356
SJY <jihoseo2008@gmail.com>
2025-05-09 11:59:40 +0900

    feat :: member location
    add marker ( member's location )

--- app/screens/member.tsx
+++ app/screens/member.tsx
@@ -5,10 +5,20 @@ function Member() {
     const center = { latitude: 37.5665, longitude: 126.978 }
     const radii = { safe: 100, warning: 200, danger: 300 }
 
+    const members = [
+        {
+            cordinate: {
+                latitude: 37.5648,
+                longitude: 126.98,
+            },
+            name: "서지유",
+        },
+    ]
+
     return (
         <>
             <Notice level={3} />
-            <Map center={center} radii={radii} />
+            <Map center={center} radii={radii} members={members} />
         </>
     )
 }
